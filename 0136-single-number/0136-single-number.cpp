1bfda244cdc06b47f2e13107611142843f04bf9f
Rushikesh Gherade <rushi422005@gmail.com>
2024-09-28 21:38:50 +0530

    Time: 7 ms (96.26%), Space: 19.6 MB (67.89%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,14 +3,14 @@ public:
     int singleNumber(vector<int>& nums) {
         int ans = 0;
 
-        // for( int val: nums){
-        //     ans = ans ^ val;
-        // }
-        // return ans;
-
-        for(int i = 0; i < nums.size(); i++){
-            ans = ans ^ nums[i];
+        for( int val: nums){
+            ans = ans ^ val;
         }
         return ans;
+
+        // for(int i = 0; i < nums.size(); i++){
+        //     ans = ans ^ nums[i];
+        // }
+        // return ans;
     }
 };

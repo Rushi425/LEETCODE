ebb640ae7e35188333ccef1dd143d706062628ec
Rushikesh Gherade <rushi422005@gmail.com>
2024-10-14 00:31:07 +0530

    Time: 0 ms (100%), Space: 8.3 MB (72.41%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -7,32 +7,11 @@ public:
         // if(x == -1 && n % 2 == 0) return 1.0;
         // if(x == -1 && n % 2 == 0) return -1.0;
 
-//         long binForm = n;
-//         //if n is negative then we have to convert 
-//         if(n < 0){                      
-//             x = 1/x;          
-//             binForm = -binForm;//binform is negative it will convert to positive
-//         }
-//         int ans = 1;
-
-//         while(binForm > 0){
-//             if(binForm % 2 == 1){
-//                 ans *= x;
-//             }
-//             x *=x;
-//             binForm /=2;
-//         }
-//     return ans;
-//     }
-// };
-
-
-
-
         long binForm = n;
+        //if n is negative then we have to convert 
         if(n < 0){
             x = 1/x;
-            binForm = -binForm;
+            binForm = -binForm;//binform is negative it will convert to
         }
 
         double ans = 1;

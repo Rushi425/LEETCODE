736e6960aa82c2e33d9ef5251dbc319906c2be58
Rushikesh Gherade <rushi422005@gmail.com>
2024-11-14 22:02:42 +0530

    Time: 0 ms (100%), Space: 8.2 MB (90.7%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int i = a.size() - 1, j = b.size() - 1;
-        string result = "";
-        int carry = 0;
+        int i = a.size() - 1, j = b.size() - 1; //calculate size;
+        string result = "";  // for result
+        int carry = 0; 
 
-        while(i >= 0 || j >= 0 || carry > 0){
+        // run till the either a = 0,b = 0 or carry = 1 is anything
+        while(i >= 0 || j >= 0 || carry > 0){        
             int sum = carry;
 
+        // convert the string charcater which is number(0,1) using '0'.
             if(i >= 0){
-                sum += a[i] - '0';
+                sum += a[i] - '0'; 
                 i--;
             }
 
@@ -17,11 +19,11 @@ public:
                 sum += b[j] - '0';
                 j--;
             }
-
+        // "+=" for appending in string directly 
             result += (sum % 2) + '0';
             carry = sum / 2;
         }
-
+        // reverse the string
         reverse(result.begin(), result.end());
         return result;
     }

62a4346d86d86b4f5b804b132e447d569486eb40
Rushikesh Gherade <rushi422005@gmail.com>
2024-11-28 10:11:36 +0530

    Time: 0 ms (100%), Space: 11.5 MB (6.41%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& v) {
         int n = v.size();
-        for(int i = n-1; i >= 0; i--){
-            if(i == n-1)
-                v[i]++;
-            if(v[i] == 10){
-                v[i] = 0;
-                if(i != 0){
-                    v[i-1]++;
-                }
-                else{
-                    v.push_back(0);
-                    v[i] = 1;
-                }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            v[i]++;  // Add 1 to the current digit
+            
+            if (v[i] < 10) {
+                return v;  // No carry
             }
+            
+            v[i] = 0;  // Carry occurred set digit to 0
         }
+        
+        // Edge case If all digits were 9 
+        v.insert(v.begin(), 1);
         return v;
     }
 };

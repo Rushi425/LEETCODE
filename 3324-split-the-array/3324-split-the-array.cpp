230778218006800580fd396d34924ede191a2e67
Rushikesh Gherade <rushi422005@gmail.com>
2025-01-10 20:53:54 +0530

    Time: 0 ms (100%), Space: 24.8 MB (82.46%) - LeetHub

--- 3324-split-the-array/3324-split-the-array.cpp
+++ 3324-split-the-array/3324-split-the-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPossibleToSplit(vector<int>& nums) {
-        int data[200] ={0};
+        int data[101] ={0};
 
         for(int i = 0; i < nums.size(); i++){
             data[nums[i]]++;

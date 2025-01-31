4140d5c033e03dcaf4e9b333c3587e178e72a773
Rushikesh Gherade <rushi422005@gmail.com>
2025-01-31 22:22:29 +0530

    Time: 1 ms (59.67%), Space: 8.7 MB (42.12%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        //for -ve no.
+        //-ve, 0s,
         if(x < 0 || (x % 10 == 0 && x != 0)){
             return false;
         }
 
         int rev = 0;
-        int original = x;
-        while(x != 0){
-            int temp = x % 10;
-
-            if(rev > INT_MAX / 10 || rev == INT_MAX / 10 && temp > 7 ){
-                return false;
-            }
-            rev = rev * 10 + temp;
+        while(x > rev){
+            rev = rev * 10 + x % 10;
             x /= 10;
         }
-        return rev == original;
+        return (x == rev || x == rev / 10);
     }
 };

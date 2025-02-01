dca61a22b4244d518a73e6f4defeb2e36bf3327a
Rushikesh Gherade <rushi422005@gmail.com>
2025-02-01 15:17:57 +0530

    Time: 51 ms (69.74%), Space: 18.7 MB (67.51%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -6,8 +6,6 @@ public:
         int row = 0, col = n - 1;
 
         while(row < m && col >= 0){
-
-
             if(target == matrix[row][col]) return true;
             else if(target < matrix[row][col]) col--;
             else row++;    

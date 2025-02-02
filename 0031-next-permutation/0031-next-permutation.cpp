7ca0c81e681f8e0327ac531398aea47b78ea40f2
Rushikesh Gherade <rushi422005@gmail.com>
2025-02-02 20:54:44 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.74%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size(), pivot = -1;
-        for(int i = n - 2; i >= 0; i--){ // pivot element 
+        // find the pivot
+        int n = nums.size();
+        int pivot = -1;
+        for(int i = n - 2; i >= 0; i--){
             if(nums[i] < nums[i + 1]){
                 pivot = i;
                 break;
             }
         }
-        if(pivot == -1) { // handling edge cases like (5,4,3,2,1)
+        //edge case
+        if(pivot == -1){
             reverse(nums.begin(), nums.end());
-            return;   
+            return;
         }
-        // Find the smallest element larger than nums[pivot] and swap
+        //find large number than pivot
         for(int i = n - 1; i > pivot; i--){
-            if(nums[i] > nums[pivot]){
-                swap(nums[i],nums[pivot]);
+            if(nums[pivot] < nums[i]){
+                swap(nums[pivot],nums[i]);
                 break;
             }
         }
-
+        //reverse part after pivot
         int a = pivot + 1, b = n - 1;
         while(a < b){
             swap(nums[a], nums[b]);
             a++;
             b--;
         }
-        
+
     }
-    
 };

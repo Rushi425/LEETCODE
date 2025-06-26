cb7632fe5f4560c36eb2c46ab843e5100d25b8d3
Rushikesh Gherade <rushi422005@gmail.com>
2025-06-26 20:23:03 +0530

    Time: 5 ms (24.08%), Space: 46.7 MB (67.96%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] countBits(int n) {
+        int [] ans = new int [n + 1];
+
+        for(int i = 0; i <= n; i++){
+            int c = 0;
+            int x = i;
+            while(x != 0){
+                if((x & 1) == 1) c++;
+                x = x>>1;
+            }
+            ans[i] = c;
+        }
+        return ans;
+    }
+}

ec56dbb285441f9e961dcea43ae2e4522ec852f4
Rushikesh Gherade <rushi422005@gmail.com>
2025-06-30 15:27:51 +0530

    Time: 0 ms (100%), Space: 40.6 MB (36.21%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,15 +1,12 @@
 class Solution {
-  public int recur(int i, int j, int m, int n, int[][] dp) {
-    if (i == m - 1 && j == n - 1) return 1;       // Reached destination
-    if (i >= m || j >= n) return 0;              // Out of bounds
-    if(dp[i][j] != -1) return dp[i][j]; //alrady solved
-    return dp[i][j] = (recur(i + 1, j,m,n,dp) + recur(i, j + 1,m,n,dp));    // Move down + Move right
-}
     public int uniquePaths(int m, int n) {
-        int [][] dp = new int[m][n];       
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        int N = m+n -2;
+        int r = m -1;
+        double res = 1;
+
+        for(int i = 1; i <= r; i++){
+            res = res * (N-r + i)/i; 
         }
-        return recur(0, 0, m, n, dp);
+        return (int)res;
     }
 }

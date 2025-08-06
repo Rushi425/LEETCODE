c3562541814157357205438b6bb0dc175a9e6877
Rushikesh Gherade <rushi422005@gmail.com>
2025-08-06 21:28:50 +0530

    Time: 0 ms (100%), Space: 40.9 MB (91.09%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,41 +1,34 @@
-import java.util.Vector;
-
 class Solution {
-    public int calculate(int []nums, int s, int e){
-        if (s == e) return nums[s];
-        // if(n == 0) return 0;
-        // if(n == 1) return arr.get(0);
 
-        // int prev = Math.max(arr.get(0), arr.get(1));
-        // int prev2 = arr.get(0);
-        // int current  = prev;
-        int prev2 = nums[s];
-        int prev = Math.max(nums[s], nums[s + 1]);
-        int current = prev;
+    static int calculate(int[] arr,int m,int n){
+        if (n - m == 0) return 0;
+        if (n - m == 1) return arr[m];
 
-
-        for(int i = s+2; i <= e; i++){
-            int take = nums[i] + prev2;
+        // int []dp = new int [n - m];
+        // dp[0] = arr[m];
+        // dp[1] = Math.max(dp[m], arr[m + 1]);
+        int prev2 = arr[m];
+        int prev = Math.max(prev2, arr[m + 1]);
+        int curr = 0;
+        for(int i = m + 2; i < n; i++){
+            // int take = dp[i - 2] + arr[i + m];
+            int take = prev2 + arr[i];
+            // int notTake = dp[i - 1];
             int notTake = prev;
-            current = Math.max(take, notTake);
+            curr = Math.max(take, notTake);
             prev2 = prev;
-            prev = current;
+            prev = curr;
         }
-        return current;
+        return prev;
     }
 
-    public int rob(int[] nums) {
-        int n = nums.length;
-        if(n == 0) return 0;
-        if(n == 1) return nums[0];
-        // Vector<Integer> temp1 = new Vector<>();
-        // Vector<Integer> temp2 = new Vector<>();
-        // for(int i = 0; i < n;i++){
-        //     if(i != 0) temp1.add(nums[i]); // exclude first house
-        //     if(i != n - 1) temp2.add(nums[i]);// exclude last house
-        // }
-        int t1 = calculate(nums, 1, n - 1);
-        int t2 = calculate(nums, 0, n - 2);
-        return Math.max(t1, t2);
+    public int rob(int[] arr) {
+        if (arr.length == 0) return 0;
+        if (arr.length == 1) return arr[0];
+
+        int startFrom0 = calculate(arr, 0, arr.length - 1);
+        int startFrom1 = calculate(arr, 1, arr.length);
+        
+        return Math.max(startFrom1, startFrom0);
     }
 }

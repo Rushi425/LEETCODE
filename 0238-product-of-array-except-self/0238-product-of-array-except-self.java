9a62bcb37d43363a7a3031561022515bbcbf18a8
Rushikesh Gherade <rushi422005@gmail.com>
2025-08-08 18:14:04 +0530

    Time: 2 ms (87.27%), Space: 55.6 MB (50.01%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,17 +1,45 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int []ans  = new int[n];
-        ans[0] =1;
-
+        int[] answer = new int[n];
+        answer[0] = 1;
         for(int i = 1; i < n; i++){
-            ans[i] = nums[i - 1] * ans[i - 1];
+            answer[i] = answer[i - 1] * nums[i - 1];
         }
         int suffix = 1;
-        for(int j = n - 2; j >= 0; j--){
-            suffix *= nums[j + 1];
-            ans[j] *= suffix;
-        }return ans;
+        for(int i = n - 2; i >= 0; i--){
+            suffix *= nums[i + 1];
+            answer[i] *= suffix;
+        }
+        return answer;      
     }
-    
 }
+   
+        // int[] answer = new int[nums.length];
+        // for(int i = 0; i < nums.length; i++){
+        //     int productOfAll = 1;
+        //     for(int j = 0; j < nums.length; j++){
+        //         if(i == j) continue;
+        //         productOfAll *= nums[j];
+        //     }
+        //     answer[i] = productOfAll;
+        // }
+        // return answer ;
+
+
+        // int[] answer = new int[nums.length];        
+        // int[] prefix = new int[nums.length];
+        // int[] suffix = new int[nums.length];
+
+        // prefix[0] = 1;
+        // for(int i = 1; i < nums.length; i++){
+        //     prefix[i] = prefix[i - 1] * nums[i - 1];
+        // }
+        // suffix[nums.length - 1] = 1;
+        // for (int i = nums.length - 2; i >= 0; i--) {
+        //     suffix[i] = suffix[i + 1] * nums[i + 1]; 
+        // }
+        // for(int i = 0; i < nums.length; i++){
+        //     answer[i] = prefix[i] * suffix[i];
+        // } 
+        // return answer ;

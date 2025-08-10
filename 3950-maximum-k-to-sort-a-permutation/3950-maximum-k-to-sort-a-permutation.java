09154fc843613c99c5a1e013aa2b1eb50958b2ac
Rushikesh Gherade <rushi422005@gmail.com>
2025-08-10 13:00:33 +0530

    Time: 1 ms (100%), Space: 62.5 MB (48.19%) - LeetHub

--- 3950-maximum-k-to-sort-a-permutation/3950-maximum-k-to-sort-a-permutation.java
+++ 3950-maximum-k-to-sort-a-permutation/3950-maximum-k-to-sort-a-permutation.java
@@ -1,17 +1,27 @@
 class Solution {
     public int sortPermutation(int[] nums) {
-        int[] copy = nums.clone();
-        Arrays.sort(copy);
-        List<Integer> arr = new ArrayList<>(); 
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] != copy[i]){
-                arr.add(i);
-            }
-        }
-        int k = Integer.MAX_VALUE;
-        for(int i: arr){
-            k &= nums[i];
-        }
-        return arr.size() > 0? k: 0;
+       int res = -1; 
+       for(int i = 0; i < nums.length; i++){
+        if(nums[i] == i) continue;
+        if(res == -1) res = nums[i];
+        else res &= nums[i];
+       }
+       if(res == -1) return 0;
+       return res;
     }
 }
+
+
+        // int[] copy = nums.clone();
+        // Arrays.sort(copy);
+        // List<Integer> arr = new ArrayList<>(); 
+        // for(int i = 0; i < nums.length; i++){
+        //     if(nums[i] != copy[i]){
+        //         arr.add(i);
+        //     }
+        // }
+        // int k = Integer.MAX_VALUE;
+        // for(int i: arr){
+        //     k &= nums[i];
+        // }
+        // return arr.size() > 0? k: 0;

c9ff41d44f7f1683a747334bf6f09dd949f94fa6
Rushikesh Gherade <rushi422005@gmail.com>
2025-08-14 21:17:57 +0530

    Time: 1 ms (99.53%), Space: 55.9 MB (52.92%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -1,33 +1,57 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        if(bills[0] < 5) return false;
-        int[] avail = {0,0};
-		
-		for(int i = 0; i < bills.length; i++) {
-			if(bills[i] == 5) {
-				avail[0] = avail[0] + 1;
-			}
-			else if (bills[i] == 10) {
-				if(avail[0] > 0) {
-                    avail[0] = avail[0] - 1;
-				    avail[1] = avail[1] + 1;
-                }else{
+        int five = 0, ten = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } 
+            else if (bill == 10) {
+                if (five-- == 0) return false;
+                ten++;
+            } 
+            else { 
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } 
+                else if (five >= 3) {
+                    five -= 3;
+                } 
+                else {
                     return false;
                 }
-			}
-			else {
-				if(avail[1] > 0 && avail[0] > 0) {
-					avail[1] = avail[1] - 1;
-					avail[0] = avail[0] - 1;
-					continue;
-				}
-				if(avail[0] > 2) {
-					avail[0] = avail[0] - 3;
-					continue;
-				}
-				return false;
-			}
+            }
         }
         return true;
     }
 }
+
+        // if(bills[0] < 5) return false;
+        // int[] avail = {0,0};
+		// for(int i = 0; i < bills.length; i++) {
+		// 	if(bills[i] == 5) {
+		// 		avail[0] = avail[0] + 1;
+		// 	}
+		// 	else if (bills[i] == 10) {
+		// 		if(avail[0] > 0) {
+        //             avail[0] = avail[0] - 1;
+		// 		    avail[1] = avail[1] + 1;
+        //         }else{
+        //             return false;
+        //         }
+		// 	}
+		// 	else {
+		// 		if(avail[1] > 0 && avail[0] > 0) {
+		// 			avail[1] = avail[1] - 1;
+		// 			avail[0] = avail[0] - 1;
+		// 			continue;
+		// 		}
+		// 		if(avail[0] > 2) {
+		// 			avail[0] = avail[0] - 3;
+		// 			continue;
+		// 		}
+		// 		return false;
+		// 	}
+        // }
+        // return true;

32895a1eee87d60a3a6c1657aeebf62abe0a0507
Rushikesh Gherade <rushi422005@gmail.com>
2025-08-15 14:40:53 +0530

    Time: 3 ms (48.11%), Space: 45.6 MB (71.15%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -4,9 +4,19 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             if(!(i > reach)){
                 reach = Math.max(reach, i + nums[i]);
+            } else{
+                return false;
             }
-            else return false;
         }
         return true;
     }
 }
+        // not valid
+        // for(int i = 1; i < nums.length; i++) {
+        //     if(max == 0) return false;
+		// 	max = max - 1;
+		// 	max = Math.max(max, nums[i]);
+		// 	if(max == 0) {
+		// 		return false;
+		// 	}
+		// }

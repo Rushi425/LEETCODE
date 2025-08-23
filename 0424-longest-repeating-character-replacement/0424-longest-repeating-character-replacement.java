2f7a5741a8014c87dbb82fe2233e982d835b114d
Rushikesh Gherade <rushi422005@gmail.com>
2025-08-23 12:06:58 +0530

    Time: 6 ms (98.36%), Space: 44.7 MB (71.29%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,18 +1,55 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int [] freq = new int[26];
-        int left = 0, max = 0, result = 0;
-
-        for(int right = 0; right < s.length(); right++){
-            freq[s.charAt(right) - 'A']++;
-            max = Math.max(max, freq[s.charAt(right) - 'A']);
-
-            if((right - left + 1) - max > k){
-                freq[s.charAt(left) - 'A']--;
+        int[] freqn = new int[26];
+        int left = 0, maxFreqn = 0, maxLen = 0;
+        for (int right = 0; right < s.length(); right++) {
+            int index = s.charAt(right) - 'A';
+            freqn[index]++;
+            maxFreqn = Math.max(maxFreqn, freqn[index]);
+            while ((right - left + 1) - maxFreqn > k) {
+                freqn[s.charAt(left) - 'A']--;
                 left++;
             }
-            result = Math.max(result, right -left+1);
+            maxLen = Math.max(maxLen, right - left + 1);
         }
-        return result;
+        return maxLen;
     }
 }
+
+
+    //    int n = s.length();
+    //    int left = 0, right = 0, maxLen = 0, maxFreqn = 0;
+    //    int[] freqn = new int[26];
+    //    while(right < n){
+    //     freqn[s.charAt(right) - 'A']++;
+    //     maxFreqn = Math.max(maxFreqn, freqn[s.charAt(right) - 'A']);
+    //     while((right - left) + 1 - maxFreqn > k){
+    //         freqn[s.charAt(left) - 'A']--;
+    //         for(int i = 0; i < 26; i++){
+    //             maxFreqn = Math.max(maxFreqn, freqn[i]);
+    //         }
+    //         left++;
+    //     }
+    //     if((right - left) - maxFreqn <= k){
+    //         maxLen = Math.max(maxLen, right - left + 1);
+    //     }
+    //     right++;
+    //    }
+    //    return maxLen;
+
+
+
+
+        // int n = s.length();
+        // int maxlen = 0;
+        // char[] charArray = str.toCharArray();
+        // for(int i = 0; i < n; i++){
+        //     int[] freqn = new int[26];
+		// 	int chnagesNeeded = 0, maxFreqn = Integer.MIN_VALUE;
+        //     for(int j = i; j < n; j++){
+		// 		freqn[charArray[j] - 'A']++;
+		// 		maxFreqn = Math.max(maxFreqn, freqn[charArray[j] - 'A']);
+        //         if(chnagesNeeded <= k) {
+		// 			maxLength = Math.max(maxLength, (j - i) + 1);	
+        //     }
+        // }

2518dcba4e06a9eb1a3b2b640eec2d97263b395d
Rushikesh Gherade <rushi422005@gmail.com>
2025-08-24 21:14:12 +0530

    Time: 6 ms (87.07%), Space: 56.6 MB (63.29%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,23 +1,41 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-    List<Integer> result = new ArrayList<>();
-
-    for (int i = 0; i < nums.length; i++) {
-        int index = Math.abs(nums[i]) - 1;  
-        if (nums[index] > 0) {
-            nums[index] = -nums[index];  
+        int n = nums.length;
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            int idx = Math.abs(nums[i]) - 1;
+            if(nums[idx] > 0){
+                nums[idx] = -nums[idx];
+            }
         }
-    }
-
-    for (int i = 0; i < nums.length; i++) {
-        if (nums[i] > 0) {
-            result.add(i + 1);  
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0) res.add(i + 1);
         }
+        return res;
     }
-
-    return result;
 }
 
-}
+        // Set<Integer> set = new HashSet<>();
+        // List<Integer> res = new ArrayList<>();       
+        // for (int num : nums) {
+        //     set.add(num);
+        // }        
+        // for (int i = 1; i <= nums.length; i++) {
+        //     if (!set.contains(i)) {
+        //         res.add(i);
+        //     }
+        // }       
+        // return res;
 
 
+        // List<Integer> res = new ArrayList<>();
+        // boolean[] seen = new boolean[nums.length + 1];       
+        // for (int num : nums) {
+        //     seen[num] = true;
+        // }
+        // for (int i = 1; i <= nums.length; i++) {
+        //     if (!seen[i]) {
+        //         res.add(i);
+        //     }
+        // }
+        // return res;
